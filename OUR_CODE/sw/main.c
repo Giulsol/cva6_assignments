4da98f9a47c3b62185deac3c6a8cfb493be6b7aa
DI LEO MARTINA <s322461@po.polito.it>
2025-01-09 16:02:32 +0100

    added Fibonacci code

--- OUR_CODE/sw/main.c
+++ OUR_CODE/sw/main.c
@@ -49,5 +49,21 @@ int main(int argc, char const *argv[])
 	sign = MISR_get_signature(start_address_misr1);
 	printf("The obtained signature is: %d\n", sign);
 
+	//Test Fibonacci code
+	int t1 = 0, t2 = 1, nextTerm = 0, n;
+	printf("Enter a positive number: ");
+	scanf("%d", &n);
+
+	// displays the first two terms which is always 0 and 1
+	printf("Fibonacci Series: %d, %d, ", t1, t2);
+	nextTerm = t1 + t2;
+
+	while (nextTerm <= n) {
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+
 	return 0;
 }
