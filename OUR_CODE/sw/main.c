d2af9282665ff7bae30e644d759ce93f0ef8c4b4
Giulsol <giuliasolito.21@gmail.com>
2025-01-10 15:59:52 +0100

    enabled second misr

--- OUR_CODE/sw/main.c
+++ OUR_CODE/sw/main.c
@@ -5,45 +5,74 @@
 
 int main(int argc, char const *argv[])
 {	
-	int rst = 0, en = 0, coeff_test = 0x0, sign = 0;
+	int rst1 = 0, en1 = 0, coeff_test1 = 0x0, sign1 = 0;
 	int coeff = 0xDDBAF555FFAAF000;
 	int start_address_misr1 = MISR_1_PERIPH_START_ADDRESS;
+	int rst2 = 0, en2 = 0, coeff_test2 = 0x0, sign2 = 0;
+	int start_address_misr2 = MISR_2_PERIPH_START_ADDRESS;
 
 	//Test Fibonacci code
 
 
-	/*		MISR reset		*/
+	/*		MISR1 reset		*/
 
 	MISR_reset(start_address_misr1);
 	printf("Reset bit of MISR #1 is being set to 0\n");
-	rst = MISR_get_reset_value(start_address_misr1);
+	rst1 = MISR_get_reset_value(start_address_misr1);
 	//polling in reading mode
-	while (rst == 0) {
+	while (rst1 == 0) {
 	}
 
 	MISR_clear_reset(start_address_misr1);
 	printf("Reset bit of MISR #1 is begin cleared\n");
-	rst = MISR_get_reset_value(start_address_misr1);
+	rst1 = MISR_get_reset_value(start_address_misr1);
 	//polling
-	while (rst == 1) {
+	while (rst1 == 1) {
+	}
+
+	/*		MISR2 reset		*/
+
+	MISR_reset(start_address_misr2);
+	printf("Reset bit of MISR #2 is being set to 0\n");
+	rst2 = MISR_get_reset_value(start_address_misr1);
+	//polling in reading mode
+	while (rst2 == 0) {
+	}
+
+	MISR_clear_reset(start_address_misr2);
+	printf("Reset bit of MISR #2 is begin cleared\n");
+	rst2 = MISR_get_reset_value(start_address_misr2);
+	//polling
+	while (rst2 == 1) {
 	}
 
 	/*		MISR coefficients setting		*/
 
 	MISR_set_coefficients(start_address_misr1, coeff);
-	printf("Setting coefficients of MISR #1\n");
-	coeff_test = MISR_get_coefficients(start_address_misr1);
+	MISR_set_coefficients(start_address_misr2, coeff);
+	printf("Setting coefficients of MISR #1 and #2\n");
+	coeff_test1 = MISR_get_coefficients(start_address_misr1);
 	//polling
-	while (coeff_test != coeff) {
+	while (coeff_test1 != coeff) {
+	}
+	coeff_test2 = MISR_get_coefficients(start_address_misr2);
+	//polling
+	while (coeff_test2 != coeff) {
 	}
 
 	/*		MISR enabling		*/
 
 	MISR_start(start_address_misr1);
 	printf("Enabling MISR #1\n");
-	en = MISR_get_enable_value(start_address_misr1);
+	en1 = MISR_get_enable_value(start_address_misr1);
 	//polling
-	while (en == 0) {
+	while (en1 == 0) {
+	}
+	MISR_start(start_address_misr2);
+	printf("Enabling MISR #2\n");
+	en2 = MISR_get_enable_value(start_address_misr2);
+	//polling
+	while (en2 == 0) {
 	}
 
 	/*		Fibonacci code		*/
@@ -66,17 +95,28 @@ int main(int argc, char const *argv[])
 	//MISR stopping
 	MISR_stop(start_address_misr1);
 	printf("Disabling MISR #1\n");
-	en = MISR_get_enable_value(start_address_misr1);
+	en1 = MISR_get_enable_value(start_address_misr1);
 	//polling
-	while(en == 1) {
+	while(en1 == 1) {
+	}
+	MISR_stop(start_address_misr2);
+	printf("Disabling MISR #2\n");
+	en2 = MISR_get_enable_value(start_address_misr2);
+	//polling
+	while(en2 == 1) {
 	}
 
 	//MISR signature retrieving
-	sign = MISR_get_signature(start_address_misr1);
+	sign1 = MISR_get_signature(start_address_misr1);
 	//polling
-	while(sign == 0) {
+	while(sign1 == 0) {
 	}
-	printf("The obtained signature is: %d\n", sign);
+	printf("The obtained signature of misr1 is: %d\n", sign1);
+	sign2 = MISR_get_signature(start_address_misr2);
+	//polling
+	while(sign2 == 0) {
+	}
+	printf("The obtained signature of misr2 is: %d\n", sign2);
 
 	return 0;
 }
