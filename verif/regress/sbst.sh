da95d37341a7971873f1432d844e4c1e54239d07
Giulsol <giuliasolito.21@gmail.com>
2025-01-13 15:43:02 +0100

    preparing the simulation

--- verif/regress/sbst.sh
+++ verif/regress/sbst.sh
@@ -3,7 +3,7 @@ if ! [ -n "$RISCV" ]; then
   echo "Error: RISCV variable undefined"
   return
 fi
-#--c_tests "$src0" \
+#--asm_tests "$src0" \
 # install the required tools
 source verif/regress/install-cva6.sh
 source verif/regress/install-riscv-dv.sh
@@ -19,13 +19,12 @@ make -C verif/sim clean_all
 
 cd verif/sim
 
-# src0=../tests/custom/sbst/sbst_main.c
- src0=../tests/custom/sbst/sbst.S
-#src0=../tests/custom/sbst/Fibonacci_test_MISR.c
+#src0=../tests/custom/sbst/sbst.S
+src0=../tests/custom/sbst/Fibonacci_test_MISR.c
 #src0=../tests/custom/sbst/fibonacci.S
 srcA=(
         ../tests/custom/common/syscalls.c
-        #../tests/custom/sbst/drivers_misr.c
+        ../tests/custom/sbst/drivers_misr.c
         # ../tests/custom/sbst/sbst.S
         ../tests/custom/common/crt.S
 )
@@ -50,6 +49,6 @@ python3 cva6.py \
         --hwconfig_opts="--default_config=cv64a6_imafdc_sv39 --isa=rv64imafdc --NrLoadPipeRegs=0" \
         --iss="$DV_SIMULATORS" \
         --iss_yaml=cva6.yaml \
-        --asm_tests "$src0" \
+        --c_tests "$src0" \
         --gcc_opts "${srcA[*]} ${cflags[*]}" \
         --linker ../tests/custom/common/test.ld 
